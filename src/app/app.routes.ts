59380d416f813873e5cf1345ff441b6ec891a00e
RomeroT76 <robertoart76x@gmail.com>
2024-05-28 00:42:37 -0500

    Adicion de la ruta de registro de clientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {path: 'login', title: 'Iniciar Sesion', component: LoginComponent},
-    {path: 'inicio', title: 'Inicio', component: InicioComponent}
+    {path: 'inicio', title: 'Inicio', component: InicioComponent},
+    {path: 'login/register', title: 'Registro', component: RegisterComponent}
 ];
