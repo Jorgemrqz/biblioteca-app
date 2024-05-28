98c1bd4c3bdddfb8d4e9af24477f187341ecd709
Jorgemrqz <estebanmarquez2702@gmail.com>
2024-05-28 08:57:03 -0500

    Asignando rutas a la pantalla de inicio y cambiando estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,6 @@ import { RegisterComponent } from './pages/register/register.component';
 export const routes: Routes = [
     {path: 'login', title: 'Iniciar Sesion', component: LoginComponent},
     {path: 'inicio', title: 'Inicio', component: InicioComponent},
-    {path: 'login/register', title: 'Registro', component: RegisterComponent}
+    {path: 'inicio/login/register', title: 'Registro', component: RegisterComponent},
+    {path: 'inicio/login', title: 'Inicio', component: LoginComponent},
 ];
