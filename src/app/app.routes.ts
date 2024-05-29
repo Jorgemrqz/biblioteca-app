21cdd797a329d1b7ff9a77b80d13ced973dfd39d
RomeroT76 <robertoart76x@gmail.com>
2024-05-29 07:46:24 -0500

    agregar funcionalidades al componentes de buscador de libros para que consuma la Api de google books

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,8 @@ export const routes: Routes = [
     {path: '', redirectTo:'inicio', pathMatch:'full'},
     {path: 'login', title: 'Iniciar Sesion', component: LoginComponent},
     {path: 'inicio', title: 'Inicio', component: InicioComponent},
+    {path: 'login/register', title: 'Registro', component: RegisterComponent},
     {path: 'inicio/login/register', title: 'Registro', component: RegisterComponent},
     {path: 'inicio/login', title: 'Inicio', component: LoginComponent},
-    {path:'**', redirectTo:'inicio', pathMatch: 'full'}
+    // {path:'**', redirectTo:'inicio', pathMatch: 'full'}
 ];
